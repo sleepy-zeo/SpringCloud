163dabe3790083cebdf48f346fe79c25b475ca29
wuhongwei <hongwei.wu@grandsun.com>
2020-11-23 19:57:07 +0800

    xx

--- cloud-eureka-client-consumer/src/main/java/com/sleepy/zeo/controller/FetchController.java
+++ cloud-eureka-client-consumer/src/main/java/com/sleepy/zeo/controller/FetchController.java
@@ -1,6 +1,5 @@
 package com.sleepy.zeo.controller;
 
-import com.sleepy.zeo.service.FeignManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -8,23 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
-/**
- * 测试发现consumer最好要在provider全部启动完成后再启动，
- * 否则有几率通过spring.application.name会出现访问失败的问题
- */
 @Controller
 @RequestMapping("/fetch")
 public class FetchController {
 
     private RestTemplate restTemplate;
     private RestTemplate restTemplate2;
-
-    private FeignManager feignManager;
-
-    @Autowired
-    public void setFeignManager(FeignManager feignManager) {
-        this.feignManager = feignManager;
-    }
 
     @Autowired
     @Qualifier("normal")
@@ -50,12 +38,5 @@ public class FetchController {
     public String detail2() {
         System.out.println("detail2");
         return restTemplate2.getForObject("http://eureka-client-provider/provider/info/detail", String.class);
-    }
-
-    @RequestMapping("/detail3")
-    @ResponseBody
-    public String detail3() {
-        System.out.println("detail3");
-        return feignManager.detail();
     }
 }
