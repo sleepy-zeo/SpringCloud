767d2a523eb3f734921fd552c373f70bd1849910
wuhongwei <hongwei.wu@grandsun.com>
2020-11-23 15:52:37 +0800

    an executive server-provider-consumer

--- cloud-eureka-client-consumer/src/main/java/com/sleepy/zeo/controller/FetchController.java
+++ cloud-eureka-client-consumer/src/main/java/com/sleepy/zeo/controller/FetchController.java
@@ -1,6 +1,7 @@
 package com.sleepy.zeo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,23 +12,31 @@ import org.springframework.web.client.RestTemplate;
 public class FetchController {
 
     private RestTemplate restTemplate;
+    private RestTemplate restTemplate2;
 
     @Autowired
+    @Qualifier("normal")
     public void setRestTemplate(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
-    /*
-    @RequestMapping("/info/detail")
+    @Autowired
+    @Qualifier("balanced")
+    public void setRestTemplate2(RestTemplate restTemplate) {
+        this.restTemplate2 = restTemplate;
+    }
+
+    @RequestMapping("/detail")
     @ResponseBody
     public String detail() {
-        return restTemplate.getForObject("http://localhost:10011/info/detail", String.class);
+        System.out.println("detail");
+        return restTemplate.getForObject("http://localhost:10033/info/detail", String.class);
     }
-    */
 
-    @RequestMapping("/info/detail2")
+    @RequestMapping("/detail2")
     @ResponseBody
     public String detail2() {
-        return restTemplate.getForObject("http://eureka-client-provider/info/detail", String.class);
+        System.out.println("detail2");
+        return restTemplate2.getForObject("http://eureka-client-provider/info/detail", String.class);
     }
 }
--- cloud-eureka-client-provider/src/main/java/com/sleepy/zeo/controller/InfoController.java
+++ cloud-eureka-client-provider/src/main/java/com/sleepy/zeo/controller/InfoController.java
@@ -1,5 +1,6 @@
 package com.sleepy.zeo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,10 +9,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/info")
 public class InfoController {
 
+    @Value("${server.port}")
+    public String port;
+
     @RequestMapping("/detail")
     @ResponseBody
     public String detail() {
-        return "detail: eureka client provider";
+        return "port: " + port + ", detail: eureka client provider";
     }
 
 }
