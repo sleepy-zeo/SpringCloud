4213e51d7d529fb31757792e59a9f56969dc50cd
Vaadhoo <steven199409@outlook.com>
2020-12-01 02:35:33 +0800

    bus

---
+++ cloud-bus-config-server/src/main/java/com/sleepy/zeo/CloudBusConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.sleepy.zeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBusConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudBusConfigServerApplication.class, args);
+    }
+
+}
