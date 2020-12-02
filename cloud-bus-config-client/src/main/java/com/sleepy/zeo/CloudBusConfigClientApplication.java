024e77595a0b1d24ec31b4c6dd7412c2c6ceb1f7
Vaadhoo <steven199409@outlook.com>
2020-12-03 03:06:56 +0800

    easy bus

--- cloud-bus-config-client/src/main/java/com/sleepy/zeo/CloudBusConfigClientApplication.java
+++ cloud-bus-config-client/src/main/java/com/sleepy/zeo/CloudBusConfigClientApplication.java
@@ -3,12 +3,14 @@ package com.sleepy.zeo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @Controller
+@RefreshScope
 public class CloudBusConfigClientApplication {
 
     public static void main(String[] args) {
