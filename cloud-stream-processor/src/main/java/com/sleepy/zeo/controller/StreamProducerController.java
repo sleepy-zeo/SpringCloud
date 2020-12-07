f6029400b1e3c7f9084d38f5a25f0f3e68c02931
Vaadhoo <steven199409@outlook.com>
2020-12-08 02:30:37 +0800

    xx

--- cloud-stream-processor/src/main/java/com/sleepy/zeo/controller/StreamProducerController.java
+++ cloud-stream-processor/src/main/java/com/sleepy/zeo/controller/StreamProducerController.java
@@ -1,11 +1,8 @@
 package com.sleepy.zeo.controller;
 
-import com.sleepy.zeo.stream.SleepySender;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Controller;
@@ -17,30 +14,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Controller
-@RequestMapping("/produce")
-@EnableBinding(value = Source.class)
+@RequestMapping("/stream")
 public class StreamProducerController {
 
     private static final Log logger = LogFactory.getLog(StreamProducerController.class);
-
-    private SleepySender sleepySender;
-
-    @Autowired
-    public void setSleepySender(SleepySender sleepySender) {
-        this.sleepySender = sleepySender;
-    }
-
-    @RequestMapping("/send")
-    public void send(HttpServletRequest request, HttpServletResponse response,
-                     @RequestParam("msg") String message) throws IOException {
-        sleepySender.serverOutput().send(MessageBuilder.withPayload(message).build());
-        response.getWriter().write("success");
-    }
-
-    @StreamListener(value = "sleepy-channel-client")
-    public void receiveMessage(Object payload) {
-        logger.info("receiveMessage, payload: " + payload);
-    }
 
     private Source source;
 
@@ -49,11 +26,10 @@ public class StreamProducerController {
         this.source = source;
     }
 
-    @RequestMapping("/source")
+    @RequestMapping("/send")
     public void sendBySource(HttpServletRequest request, HttpServletResponse response,
                              @RequestParam("msg") String message) throws IOException {
         source.output().send(MessageBuilder.withPayload(message).build());
         response.getWriter().write("success");
     }
-
 }
--- cloud-stream-processor/src/main/java/com/sleepy/zeo/receiver/SinkReceiver.java
+++ cloud-stream-processor/src/main/java/com/sleepy/zeo/receiver/SinkReceiver.java
@@ -1,27 +1,23 @@
 package com.sleepy.zeo.receiver;
 
-import com.sleepy.zeo.stream.SleepyReceiver;
-import com.sleepy.zeo.stream.SleepySender;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.cloud.stream.messaging.Source;
 
 // EnableBinding主要有两个功能:
 // 1. 被注解的类成为一个Configuration，即该类的实例被Spring容器管理
 // 2. value对应的类会成为一个Bean，被注入到Spring容器中
-@EnableBinding(value = {SleepySender.class, SleepyReceiver.class})
-public class ServerReceiver {
+@EnableBinding(value = {Sink.class, Source.class})
+public class SinkReceiver {
 
     private static final Log logger = LogFactory.getLog(SinkReceiver.class);
 
     // StreamListener指定了监听的消息通道名
-    // SendTo指定返回内容的输出通道
-    @StreamListener(value = "sleepy-channel-server")
-    @SendTo(value = "sleepy-channel-client")
-    public Object receiveMessage(Object payload) {
+    @StreamListener(value = Sink.INPUT)
+    public void receiveMessage(Object payload) {
         logger.info("receiveMessage, payload: " + payload);
-        return "message for client: parsed success";
     }
 }
